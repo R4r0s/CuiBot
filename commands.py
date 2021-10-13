ca879d17cbe0c79d9acfc6654936e7afad9a03e2
Mikel Fuentes <mikel.fuentes@live.u-tad.com>
2021-10-13 22:22:56 +0200

    Stop and error handleing

--- commands.py
+++ commands.py
@@ -1,3 +1,5 @@
+from asyncio import sleep
+
 import discord
 from discord.ext import commands
 from discord.utils import get
@@ -6,7 +8,7 @@ from youtube_dl import YoutubeDL
 from random import randint
 
 bot = commands.Bot(command_prefix="!", help_command=None)
-TOKEN = "ODkwMzYwMzM1NjgzNDI0MjU3.YUuqrg.Q68KMXR_iWESOBC9LdTLYlvQiJA"
+TOKEN = open("TOKEN.txt").read()
 
 
 @bot.event
@@ -15,21 +17,19 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=custom)
 
 
-@bot.command()
+@bot.command(pass_context=True)
 async def random(ctx, arg):
     try:
         if int(arg) == 420:
             await ctx.send(69)
         else:
-
             await ctx.send(str(randint(0, int(arg))))
 
-    except:
-
+    except ValueError:
         await ctx.send("Only numbers are accepted")
 
 
-@bot.command()
+@bot.command(pass_context=True)
 async def roll(ctx, arg):
     if arg.lower() == "d4":
         await ctx.send(str(randint(1, 4)))
@@ -49,29 +49,42 @@ async def roll(ctx, arg):
         await ctx.send("Incorrect dice type please use d4, d6, d8, d10, d12, d20 or d100")
 
 
-@bot.command()
+@bot.command(pass_context=True)
 async def cuicui(ctx):
     await ctx.send("Pees itself")
 
 
 @bot.command(pass_context=True)
 async def play(ctx, arg):
-    channel = ctx.author.voice.channel
-    await channel.connect()
+    try:
+        channel = ctx.author.voice.channel
+        await channel.connect()
 
-    ybdl_options = {'format': 'bestaudio', 'noplaylist': 'True'}
-    ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        ybdl_options = {'format': 'bestaudio', 'noplaylist': 'True'}
+        ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        voice = get(bot.voice_clients, guild=ctx.guild)
+
+        if not voice.is_playing():
+            with YoutubeDL(ybdl_options) as ydl:
+                info = ydl.extract_info(arg, download=False)
+            url = info['formats'][0]['url']
+            voice.play(FFmpegPCMAudio(url, **ffmpeg_options))
+            voice.is_playing()
+            while voice.is_playing():
+                await sleep(1)
+            await voice.disconnect()
+        else:
+            await ctx.send("Already playing song")
+            return
+
+    except AttributeError:
+        await ctx.send("You are not in a voice channel")
+
+
+@bot.command(pass_context=True)
+async def stop(ctx):
     voice = get(bot.voice_clients, guild=ctx.guild)
-
-    if not voice.is_playing():
-        with YoutubeDL(ybdl_options) as ydl:
-            info = ydl.extract_info(arg, download=False)
-        url = info['formats'][0]['url']
-        voice.play(FFmpegPCMAudio(url, **ffmpeg_options))
-        voice.is_playing()
-    else:
-        await ctx.send("Already playing song")
-        return
+    await voice.disconnect()
 
 
 @bot.command(pass_context=True)
@@ -85,6 +98,7 @@ async def help(ctx):
     embed.add_field(name="!roll", value="Rolls a desired dice type (d4, d6, d8, d10, d12, d20 and d100)", inline=False)
     embed.add_field(name="!cuicui", value="Such a dirty guinea pig", inline=False)
     embed.add_field(name="!play", value="Plays audio from URL", inline=False)
+    embed.add_field(name="!stop", value="Disconnects bot from channel", inline=False)
 
     await author.send(embed=embed)
 
