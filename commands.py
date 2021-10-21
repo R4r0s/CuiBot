8fe6c799657f53a499356b89dd95ef026168a49c
R4r0s <mikel.aingeru.fuentes@gmail.com>
2021-10-21 16:41:08 +0200

    play fix, skip fix, jokes fix, help update, new text formatting and aliases

--- commands.py
+++ commands.py
@@ -57,63 +57,70 @@ async def cuicui(ctx):
     await ctx.send("Pees itself")
 
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True, aliases=['p'])
 async def play(ctx, arg):
     try:
-        voice = get(bot.voice_clients, guild=ctx.guild)
 
         if not queue:
             queue.append(arg)
             await ctx.send("Song added to queue")
             channel = ctx.author.voice.channel
             await channel.connect()
+            voice = get(bot.voice_clients, guild=ctx.guild)
 
         if not voice.is_playing():
             while queue:
                 ybdl_options = {'format': 'bestaudio', 'noplaylist': 'True'}
                 ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-                await ctx.send("Playing song")
                 with YoutubeDL(ybdl_options) as ydl:
                     info = ydl.extract_info(queue[0], download=False)
+                title = info.get("title", None)
+                await ctx.send("Playing " + "***" + title + "***")
                 url = info['formats'][0]['url']
                 voice.play(FFmpegPCMAudio(url, **ffmpeg_options))
                 voice.is_playing()
-                while voice.is_playing():
-                    await sleep(1)
                 queue.pop(0)
+                while voice.is_playing:
+                    await sleep(1)
             await voice.disconnect()
 
         else:
             queue.append(arg)
-            for i in range(len(queue)):
-                await ctx.send(queue[i])
+            with YoutubeDL(ybdl_options) as ydl:
+                info = ydl.extract_info(arg, download=False)
+            title = info.get("title", None)
+            await ctx.send("***" + title + "***" + " added to the queue")
             return
 
     except AttributeError:
         await ctx.send("You are not in a voice channel")
 
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True, aliases=['st'])
 async def stop(ctx):
     voice = get(bot.voice_clients, guild=ctx.guild)
     await voice.disconnect()
     queue.clear()
 
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True, aliases=['s'])
 async def skip(ctx):
     voice = get(bot.voice_clients, guild=ctx.guild)
-    if queue[0]:
+    if not queue:
+        await ctx.send("Nothing to skip")
+
+    else:
         await ctx.send("Skipping song")
         ybdl_options = {'format': 'bestaudio', 'noplaylist': 'True'}
         ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         voice.stop()
         with YoutubeDL(ybdl_options) as ydl:
-            info = ydl.extract_info(queue[0], download=False)
+           info = ydl.extract_info(queue[0], download=False)
+        title = info.get("title", None)
+        await ctx.send("Playing " + "***" + title + "***")
         url = info['formats'][0]['url']
         voice.play(FFmpegPCMAudio(url, **ffmpeg_options))
-    else:
-        await ctx.send("Nothing to skip")
+        queue.pop(0)
 
 @bot.command(pass_context=True)
 async def joke(ctx):
@@ -123,23 +130,27 @@ async def joke(ctx):
     joke_json = json.dumps(response.json())
     print(response.json().get('setup'))
     print(response.json().get('delivery'))
-    await ctx.send(response.json().get('setup'))
+    if response.json().get('joke'):
+        await ctx.send("**" + response.json().get('joke') + "**")
+    if response.json().get('setup'):
+        await ctx.send("**" + response.json().get('setup') + "**")
     if response.json().get('delivery'):
-        await  ctx.send(response.json().get('delivery'))
+        await  ctx.send("**" + response.json().get('delivery') + "**")
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True, aliases=['h'])
 async def help(ctx):
     author = ctx.message.author
     embed = discord.Embed(
         colour=discord.Colour.purple()
     )
     embed.set_author(name="CuiBot's Help")
+    embed.add_field(name="!help, !h", value="Shows this list.", inline=False)
     embed.add_field(name="!random", value="Returns a random number between 0 and stated number.", inline=False)
     embed.add_field(name="!roll", value="Rolls a desired dice type (d4, d6, d8, d10, d12, d20 and d100).", inline=False)
     embed.add_field(name="!cuicui", value="Such a dirty guinea pig.", inline=False)
-    embed.add_field(name="!play", value="Plays audio from URL, if currently playing adds audio tu queue.", inline=False)
-    embed.add_field(name="!stop", value="Disconnects bot from channel, clears queue and stops playing audio.", inline=False)
-    embed.add_field(name="!skip", value="Skips current song.", inline=False)
+    embed.add_field(name="!play, !p", value="Plays audio from URL, if currently playing adds audio tu queue.", inline=False)
+    embed.add_field(name="!stop, !st", value="Disconnects bot from channel, clears queue and stops playing audio.", inline=False)
+    embed.add_field(name="!skip, !s", value="Skips current song.", inline=False)
     embed.add_field(name="!joke", value="Sends a random joke from JokeAPI (NSFW).", inline=False)
 
     await author.send(embed=embed)
